c06dd824534f6554793a12f1cb2756b0e5dbff96
Artemii Oliinyk <aoliinyk2022@gmail.com>
2023-08-10 15:25:33 -0400

    Make home_page.dart adaptive

--- lib/feature/home/cubit/home_state.dart
+++ lib/feature/home/cubit/home_state.dart
@@ -4,5 +4,6 @@ part of 'home_cubit.dart';
 class HomeState with _$HomeState {
   const factory HomeState.general({
     @Default([]) List<Character> characters,
+    Character? selected,
   }) = _Success;
 }
