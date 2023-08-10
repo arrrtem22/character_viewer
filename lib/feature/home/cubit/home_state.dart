748b7208c7f011494882202ca6813f67c288d7fe
Artemii Oliinyk <aoliinyk2022@gmail.com>
2023-08-10 13:30:29 -0400

    Implement search

--- lib/feature/home/cubit/home_state.dart
+++ lib/feature/home/cubit/home_state.dart
@@ -2,13 +2,7 @@ part of 'home_cubit.dart';
 
 @freezed
 class HomeState with _$HomeState {
-  const factory HomeState.initial() = _Initial;
-
-  const factory HomeState.loading() = _Loading;
-
-  const factory HomeState.success({
-    required Characters characters,
+  const factory HomeState.general({
+    @Default([])List<Topic> characters,
   }) = _Success;
-
-  const factory HomeState.failure() = _Failure;
 }
