2248718428dc7572f505db79aebbc8fefa0d2f2b
Artemii Oliinyk <aoliinyk2022@gmail.com>
2023-08-10 15:09:40 -0400

    Add image in detail_page.dart

--- lib/common/network/dto/character.dart
+++ lib/common/network/dto/character.dart
@@ -10,13 +10,13 @@ class Character with _$Character {
   // very strange api
   const factory Character({
     @JsonKey(name: 'FirstURL', fromJson: _parseTitle) required String title,
-    @JsonKey(name: 'Icon', fromJson: _parseIcon) String? iconUrl,
+    @JsonKey(name: 'Icon', fromJson: _parseIcon) required String imageUrl,
     @JsonKey(name: 'Text') required String description,
   }) = _Character;
 
   factory Character.fromJson(Map<String, Object?> json) => _$CharacterFromJson(json);
 }
 
-String? _parseIcon(dynamic json) => json != null ? json['URL'] : null;
+String _parseIcon(dynamic json) => json != null ? json['URL'] : '';
 
 String _parseTitle(String json) => json.split('/').last;
